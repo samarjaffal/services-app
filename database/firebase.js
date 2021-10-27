6393e3d171550e8e44d1c5a4b906f33d7f8d2389
samarjaffal <samarjaffalh@gmail.com>
2021-10-27 08:13:50 -0400

    added env files

--- database/firebase.js
+++ database/firebase.js
@@ -1,3 +1,4 @@
+import { AUTH_DOMAIN, API_KEY, PROJECT_ID, STORAGE_BUCKET, MESSANGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from '@env'
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase'
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,13 +8,13 @@ import 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAJHtOnLLlxnv_AcjGAspC3i4yoXK5fC0A',
-  authDomain: 'servicesapp-9b8a0.firebaseapp.com',
-  projectId: 'servicesapp-9b8a0',
-  storageBucket: 'servicesapp-9b8a0.appspot.com',
-  messagingSenderId: '490157409790',
-  appId: '1:490157409790:web:e26edcf2f5c49ed69a2caf',
-  measurementId: 'G-Y5X6055K33'
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSANGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
 }
 
 // Initialize Firebase
